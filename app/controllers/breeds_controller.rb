fbe38bf90264cc63ee717d56b5d9eb6eb83d0721
Alireza Andersen <alireza.andersen@gmail.com>
2018-07-23 15:44:19 -0600

    updated controller

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,7 +1,11 @@
 class BreedsController < ApplicationController
-
   def index
     @breed = DogBreedFetcher.fetch
+  end
+
+  def show_image
+    dog_pic = DogBreedFetcher.fetch_picture(params[:type])
+    render :json => dog_pic
   end
 
 end
