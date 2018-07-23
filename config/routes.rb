e95b1561123d437e1ce4143d185f20dacaf5b77e
Alireza Andersen <alireza.andersen@gmail.com>
2018-07-23 15:36:02 -0600

    created get api route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :breeds
 
   root "breeds#index"
-
+  get '/show_image/:type' => 'breeds#show_image'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
