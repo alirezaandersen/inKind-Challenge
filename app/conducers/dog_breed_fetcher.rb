1ce8a1754086550986b722e586314a02a772901c
Alireza Andersen <alireza.andersen@gmail.com>
2018-07-23 15:48:57 -0600

    logic updated

--- app/conducers/dog_breed_fetcher.rb
+++ app/conducers/dog_breed_fetcher.rb
@@ -1,36 +1,40 @@
 class DogBreedFetcher
   attr_reader :breed
 
-  def initialize(name=nil)
-    @name  = name || "random"
-    @breed = Breed.find_or_initialize_by(name: name)
+  def initialize(name="random")
+    @name  = name
   end
 
   def fetch
-    return @breed if @breed.pic_url.present?
-
-    @breed.pic_url = fetch_info["message"]
-    @breed.save && @breed
+    @breed = dog_breeds
   end
 
-  def self.fetch(name=nil)
-    name ||= "random"
+  def self.fetch(name="random")
     DogBreedFetcher.new(name).fetch
   end
 
-private
-  def fetch_info
-    begin
-      JSON.parse(RestClient.get("https://dog.ceo/api/breeds/image/#{ @name }").body)
-    rescue Object => e
-      default_body
+  private
+
+  def dog_breeds
+    canine = []
+    fetch_names["message"].each do |breed, sub_breed|
+      if sub_breed.empty?
+        canine << breed
+      else
+        sub_breed.reverse.each do |x|
+          canine << "#{breed}"+"-"+"#{x}"
+        end
+      end
     end
+    canine
   end
 
-  def default_body
-    {
-      "status"  => "success",
-      "message" => "https://images.dog.ceo/breeds/cattledog-australian/IMG_2432.jpg"
-    }
+  def self.fetch_picture(canine)
+    RestClient.get("https://dog.ceo/api/breed/#{canine.downcase}/images/random")
   end
+
+  def fetch_names
+    JSON.parse(RestClient.get("https://dog.ceo/api/breeds/list/all").body)
+  end
+
 end
